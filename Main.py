d7fda251a5f7b4b62df3e7609fd837178946f8db
FalconChrome <57348251+FalconChrome@users.noreply.github.com>
2019-11-19 11:39:33 +0300

    New1

--- Main.py
+++ Main.py
@@ -1,9 +1,9 @@
 import sys
 from random import randint
 
-from PyQt5 import uic
-from PyQt5.QtGui import QPainter, Qt
+from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QWidget
+from Ui import
 
 SCREEN_SIZE = [400, 400]
 
@@ -11,7 +11,7 @@ SCREEN_SIZE = [400, 400]
 class Circles(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('Ui.ui',self)
+        self.setupUi()
         self.initUi()
 
     def initUi(self):
@@ -22,7 +22,7 @@ class Circles(QWidget):
     def paint(self):
         qp = QPainter()
         qp.begin(self)
-        qp.setBrush(Qt.yellow)
+        qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
         qp.drawEllipse(randint(50, SCREEN_SIZE[0]),
                       randint(50, SCREEN_SIZE[1]), *(randint(1, 100),) * 2)
         qp.end()
